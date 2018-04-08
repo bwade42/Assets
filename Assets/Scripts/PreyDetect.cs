68931487a7c2b86083b95707621c71ffce2c2480
Brandon Wade <bwade@unm.edu>
2018-04-08 15:48:43 -0600

    Revamped Everything
    -Made significant changes to the design of the scene
    - Added both the wolf and deer models
    - New Folder were all the most updated scenes and scripts will go into is called "Predator vs Prey"

---
+++ Assets/Scripts/PreyDetect.cs
@@ -0,0 +1,30 @@
+﻿
+//Detect when the prey has got "eaten/touched" by the predator. 
+//Detect when the prey has touched an obstacle. 
+//Put this script onto the prey. There's nothing you need to set in the editor.
+//Make sure the prey is tagged with prey in the editor.
+
+using UnityEngine;
+using System.Collections;
+
+public class PreyDetect : MonoBehaviour
+{
+	[HideInInspector]
+	/// <summary>
+	/// The associated agent.
+	/// This will be set by the agent script on Initialization. 
+	/// Don't need to manually set.
+	/// </summary>
+	public WolfAgent agent;  //
+
+	void OnCollisionEnter(Collision col)
+	{
+		// Touched prey
+		if (col.gameObject.CompareTag("goal"))
+		{
+			agent.IScoredAGoal();
+		}
+	}
+
+}
+
